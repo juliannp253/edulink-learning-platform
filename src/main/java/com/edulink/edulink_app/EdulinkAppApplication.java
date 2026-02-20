8c17d7e8f9c5f5612edee5efc707e9296a8ddf9e
Julian Padron <julian.padronnunez03@gmail.com>
2026-02-20 13:33:34 -0600

    feat: initial project setup with spring boot and dependencies

---
+++ src/main/java/com/edulink/edulink_app/EdulinkAppApplication.java
@@ -0,0 +1,13 @@
+package com.edulink.edulink_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdulinkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdulinkAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edulink/edulink_app/EdulinkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edulink.edulink_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdulinkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
